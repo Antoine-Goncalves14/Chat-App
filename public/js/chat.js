0cec26ad6276b4489bf9a48e249cbd4ba6b2cb9c
Gonçalves Antoine <antoine.goncalves-isc.france@ibm.com>
2021-05-21 16:33:38 +0200

    add geoloca and pop if user connect

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,10 +1,26 @@
 const socket = io();
 
-socket.on('countUpdated', (count) => {
-    console.log('The count has been updated!', count);
+socket.on('message', (msg) => {
+    console.log(msg);
 });
 
-document.querySelector('#increment').addEventListener('click', () => {
-    console.log('Clicked');
-    socket.emit('increment');
+document.querySelector('#message-form').addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const message = document.querySelector('input').value;
+
+    socket.emit('sendMessage', message);
+});
+
+document.querySelector('#send-location').addEventListener('click', () => {
+    if (!navigator.geolocation) {
+        return alert('Geolocation is not supported by your browser');
+    }
+
+    navigator.geolocation.getCurrentPosition((position) => {
+        socket.emit('sendLocation', {
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude
+        });
+    });
 });
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const path = require('path');
 const http = require('http');
 const express = require('express');
 const socketIo = require('socket.io');
+const ms = require('ms');
 
 const app = express();
 const server = http.createServer(app);
@@ -12,17 +13,22 @@ const publicDirectoryPath = path.join(__dirname, '../public');
 
 app.use(express.static(publicDirectoryPath));
 
-let count = 0;
-
 io.on('connection', (socket) => {
     console.log('New WebSocket connection');
 
-    socket.emit('countUpdated', count);
+    socket.emit('message', 'Welcome!');
+    socket.broadcast.emit('message', 'A new user has joined!');
 
-    socket.on('increment', () => {
-        count++;
+    socket.on('sendMessage', (msg) => {
+        io.emit('message', msg);
+    });
 
-        io.emit('countUpdated', count);
+    socket.on('sendLocation', (coords) => {
+        io.emit('message', `https://google.com/maps?q=${coords.latitude},${coords.longitude}`);
+    });
+
+    socket.on('disconnect', () => {
+        io.emit('message', 'A user has left!');
     });
 });
 
